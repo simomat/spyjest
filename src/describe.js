7a06c6f35a4b98125db863a374bb884bfedb4c6b
simomat <simon@infonautika.de>
2017-05-09 19:24:58 +0000

    better no-match description and simplification

--- src/describe.js
+++ src/describe.js
@@ -1,43 +1,66 @@
-
-class Describer {
-    constructor() {
-        this._numCalls = NaN;
-        this._withArgs = [];
+const zipLimitFirst = (first, second) => {
+    let zipped = [];
+    for (let i = 0; i < first.length; i++) {
+        zipped.push([first[i], second[i]]);
     }
+    return zipped;
+};
 
-    times(calls) {
-        this._numCalls = calls;
-        return this;
-    }
+const noMatchingCall = (calls, expectedArgs) =>
+    ! calls.some(actualArgs =>
+        zipLimitFirst(expectedArgs, actualArgs)
+            .every(([expected, actual]) => expected.matches(actual)));
 
-    withArgs(args) {
-        this._withArgs = args;
-        return this;
-    }
+const matchingCallsCount = (calls, expectedArgs) =>
+    calls.map(call =>
+        zipLimitFirst(expectedArgs, call)
+            .every(([expected, actual]) => expected.matches(actual)))
+        .reduce((count, matched) => matched ? count + 1 : count, 0);
 
-    describeTo(description) {
-        description.append('a function that was ');
 
-        if (this._numCalls === 0) {
-            description.append('not ')
-        }
-        description.append('called');
-
-        if (Number.isInteger(this._numCalls) && this._numCalls > 0) {
-            description.append(` ${this._numCalls} times`);
-        }
-
-        if (this._withArgs.length > 0) {
-            description.append(' with arguments: ');
-            for (let i = 0; i < this._withArgs.length -1; i++) {
-                this._withArgs[i].describeTo(description);
-                description.append(', ');
+export const getMismatchDescription = (actual, expectedArgs, expectedCount) => {
+    if (expectedArgs.length === 0) {
+        if (isNaN(expectedCount)) {
+            if (actual.__calls.length === 0) {
+                return 'was not called';
             }
-            this._withArgs[this._withArgs.length-1].describeTo(description);
+        } else {
+            if (actual.__calls.length !== expectedCount) {
+                return `was called ${actual.__calls.length} times`;
+            }
+        }
+    } else {
+        if (isNaN(expectedCount)) {
+            if (noMatchingCall(actual.__calls, expectedArgs)) {
+                return 'was not called with expected arguments';
+            }
+        } else {
+            let matchingCount = matchingCallsCount(actual.__calls, expectedArgs);
+            if (matchingCount !== expectedCount) {
+                return `was called ${matchingCount} times with expected arguments`;
+            }
         }
     }
-}
+};
 
-export function aFunctionThatWasCalled() {
-    return new Describer();
-}
+export const describeMatcher = (numCalls, withArgs, description) => {
+    description.append('a function that was');
+
+    if (numCalls === 0) {
+        description.append(' not')
+    }
+    description.append(' called');
+
+    if (Number.isInteger(numCalls) && numCalls > 0) {
+        description.append(` ${numCalls} times`);
+    }
+
+    if (withArgs.length > 0) {
+        description.append(' with arguments: ');
+        for (let i = 0; i < withArgs.length - 1; i++) {
+            withArgs[i].describeTo(description);
+            description.append(', ');
+        }
+        withArgs[withArgs.length - 1].describeTo(description);
+    }
+};
--- src/functionmatcher.js
+++ src/functionmatcher.js
@@ -1,6 +1,4 @@
-import {Description} from 'hamjest';
-import {applyMatchers} from './applymatchers';
-import {aFunctionThatWasCalled} from "./describe";
+import {describeMatcher, getMismatchDescription} from './describe';
 
 export class FunctionMatcher {
     constructor(argMatchers) {
@@ -8,38 +6,24 @@ export class FunctionMatcher {
         this.expectedCount = NaN;
     }
 
-    matches(actual) {
-        return applyMatchers(actual, this.expectedArgs, this.expectedCount).match;
-    }
-
-    describeTo(description) {
-        aFunctionThatWasCalled()
-            .times(this.expectedCount)
-            .withArgs(this.expectedArgs)
-            .describeTo(description);
-    }
-
-    describeMismatch(actual, description) {
-        let matchResult = applyMatchers(actual, this.expectedArgs, this.expectedCount);
-        if (! matchResult.match) {
-            matchResult.actualDescribe.describeTo(description);
-        }
-    }
-
-    getExpectedForDiff() {
-        let description = new Description();
-        this.describeTo(description);
-        return description.get();
-    }
-
-    formatActualForDiff(actual) {
-        const description = new Description();
-        this.describeMismatch(actual, description);
-        return description.get();
-    }
-
     times(count) {
         this.expectedCount = Number.parseInt(count);
         return this;
+    }
+
+    matches(actual) {
+        return ! Boolean(this._getMismatch(actual));
+    }
+
+    describeTo(description) {
+        return describeMatcher(this.expectedCount,  this.expectedArgs, description);
+    }
+
+    describeMismatch(actual, description) {
+        description.append(this._getMismatch(actual));
+    }
+
+    _getMismatch(actual) {
+        return getMismatchDescription(actual, this.expectedArgs, this.expectedCount);
     }
 }
